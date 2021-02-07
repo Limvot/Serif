e9e5bdd858f8ce8e50948967d636b2ff62790a16
Marcus "Mark" Godwin <godwin3@gatech.edu>
2021-02-06 20:27:38 -0500

    script fails if test dir already exists.

--- run_dist.sh
+++ run_dist.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-./gradlew :serif_cli:assembleDist && mkdir test && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
+
+#ensure test directory exists
+if [ ! -d ./test ]; then
+    mkdir test
+fi
+./gradlew :serif_cli:assembleDist && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
