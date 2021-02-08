ac17ea055d0913194eaa0476fcc88a7fd6f984eb
Marcus "Mark" Godwin <godwin3@gatech.edu>
2021-02-08 08:54:35 -0500

    Switch to using the p option for mkdir instead.

--- run_dist.sh
+++ run_dist.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-#make test dir if it doesn't exist
-if [ ! -d test ]; then
-    mkdir test
-fi
-
-./gradlew :serif_cli:assembleDist && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
+./gradlew :serif_cli:assembleDist && mkdir -p test && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
