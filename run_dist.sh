40bfb34e93b2133f6728091e5be76bc4710af201
Marcus "Mark" Godwin <godwin3@gatech.edu>
2021-02-07 23:21:00 -0500

    Don't fail if test dir already exists. Ignore test dir.

--- run_dist.sh
+++ run_dist.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-./gradlew :serif_cli:assembleDist && mkdir test && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
+
+#make test dir if it doesn't exist
+if [ ! -d test ]; then
+    mkdir test
+fi
+
+./gradlew :serif_cli:assembleDist && cd test && tar xf ../serif_cli/build/distributions/serif_cli.tar && ./serif_cli/bin/serif_cli && cd .. && rm -r test
