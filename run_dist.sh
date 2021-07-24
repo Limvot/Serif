87bbc6ac1784e7b6b204cdbb6984c12a96d7174d
Neel Laptop <neelbhalani@gmail.com>
2021-07-24 15:57:09 -0400

    fixed small issues

--- run_dist.sh
+++ run_dist.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-export TERM=cygwin
 target=serif_swing
 tar_loc=../$target/build/distributions/$target.tar
-./gradlew --console=plain :$target:assembleDist && mkdir -p test && cd test && tar xf $tar_loc && ./$target/bin/$target && cd .. && rm -r test
+./gradlew :$target:assembleDist && mkdir -p test && cd test && tar xf $tar_loc && ./$target/bin/$target && cd .. && rm -r test
