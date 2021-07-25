2d485c84e9ed6624f5473d7010278200374e3343
Nathan Braswell <miloignis@gmail.com>
2021-07-24 22:17:58 -0400

    Quick recycling fix for displayname

--- serif_swing/src/main/kotlin/serif_swing/App.kt
+++ serif_swing/src/main/kotlin/serif_swing/App.kt
@@ -699,8 +699,8 @@ class SwingChatRoom(val transition: (MatrixState, Boolean) -> Unit, val panel: J
         menu_item.setText(pin_str)
     }
     val mk_sender = { msg: SharedUiMessage ->
-        val displayname = msg.displayname ?: msg.sender
         val render_text = { msg: SharedUiMessage ->
+            val displayname = msg.displayname ?: msg.sender
             if (msg.reactions.size > 0 ) {
                 "${displayname}: ${msg.reactions} "
             } else {
